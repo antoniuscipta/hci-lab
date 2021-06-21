bb76e48e4a53481da3e84060a46bfb0eebfc207f
Kendricko-adrio <kendrickoadrio134@gmail.com>
2021-06-21 22:20:39 +0700

    image slider sudah kelar

--- js/animate.js
+++ js/animate.js
@@ -1,5 +1,55 @@
-$(function(){
 
-    $("li").hide()
 
-})
+$(document).ready(() => {
+    let index = 0;
+    let slider = initSlider();
+    $('#next').click(function (e) { 
+        e.preventDefault();
+        index = nextSlide(slider, index);
+    });
+    $('#prev').click(function (e) { 
+        e.preventDefault();
+        index = prevSlide(slider, index);
+     });
+});
+
+function nextSlide(slider, index){
+    $(slider[index]).removeClass('show');
+    $(slider[index]).addClass('hide');
+    let nextIdx = (index+1) % slider.length;
+    $(slider[index]).fadeOut('slow', function(){
+        $(slider[nextIdx]).fadeIn('slow');
+    })
+    
+    
+    // $(slider[nextIdx]).removeClass('hide');
+    // $(slider[nextIdx]).addClass('show');
+    return nextIdx;
+}
+
+function prevSlide(slider, index){
+    // $(slider[index]).removeClass('show');
+    // $(slider[index]).addClass('hide');
+    let nextIdx = (index-1);
+    if(nextIdx < 0)
+    {
+        nextIdx = slider.length - 1;
+    }
+    console.log(nextIdx);
+    $(slider[index]).fadeOut('slow', function(){
+        $(slider[nextIdx]).fadeIn('slow');
+    })
+    // $(slider[nextIdx]).removeClass('hide');
+    // $(slider[nextIdx]).addClass('show');
+    return nextIdx;
+}
+
+function initSlider(){
+    console.log("test");
+    let slider = $('.slider-detail');
+    slider.addClass('hide');
+    $(slider[0]).removeClass('hide');
+    $(slider[0]).addClass('show');
+    console.log(slider);
+    return slider;
+}
